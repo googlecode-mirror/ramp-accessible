e5d6974060590af6c77c12336222d3a7932f2cd3
nscott1989 <nscott1989@6e07c496-2c5c-327b-09e2-c48372c7ecd4>
2010-12-16 11:23:26 +0000

    Added code for file upload/download. Completely untested.

--- js/files.js
+++ js/files.js
@@ -10,12 +10,18 @@ downloadElements[7] = "cboOperation";
 downloadElements[8] = "lblMachine";
 downloadElements[9] = "cboMachine";
 
-
 var uploadElements = new Array();
 uploadElements[0] = "lblCodeFile";
 uploadElements[1] = "ofdCodeFile";
 uploadElements[3] = "lblGeometryFile";
 uploadElements[4] = "ofdGeometryFile";
+
+var errorElements = new Array();
+errorElements[0] = "customerMessage";
+errorElements[1] = "partNumberMessage";
+errorElements[2] = "revisionMessage";
+errorElements[3] = "operationMessage";
+errorElements[4] = "machineMessage";
 
 
 function body_OnLoad()
@@ -25,15 +31,14 @@ function body_OnLoad()
 	document.getElementById('message').style.visibility = "hidden";
 	
 	for (element in downloadElements)
-	{	
-		document.getElementById(downloadElements[element]).style.visibility = "hidden";
-	}
+        document.getElementById(downloadElements[element]).style.visibility = "hidden";
 	
 	for (element in uploadElements)
-	{	
-		document.getElementById(uploadElements[element]).style.visibility = "hidden";
-	}
-	
+        document.getElementById(uploadElements[element]).style.visibility = "hidden";
+    
+    for (element in errorElements)
+        document.getElementById(errorElements[element]).style.visibility = "hidden";
+        
 	document.getElementById('btnSubmit').style.visibility = "hidden";
 }
 
@@ -44,14 +49,10 @@ function chooseDownload_Click()
 	document.getElementById('message').style.visibility = "visible";
 
 	for (element in downloadElements)
-	{	
-		document.getElementById(downloadElements[element]).style.visibility = "visible";
-	}
+        document.getElementById(downloadElements[element]).style.visibility = "visible";
 	
 	for (element in uploadElements)
-	{	
-		document.getElementById(uploadElements[element]).style.visibility = "hidden";
-	}
+        document.getElementById(uploadElements[element]).style.visibility = "hidden";
 	
 	document.getElementById('btnSubmit').style.visibility= "visible";
 	document.getElementById('btnSubmit').value = "Download";
@@ -64,16 +65,55 @@ function chooseUpload_Click()
 	document.getElementById('message').style.visibility = "visible";
 	
 	for (element in downloadElements)
-	{	
-		document.getElementById(downloadElements[element]).style.visibility = "visible";
-	}
+        document.getElementById(downloadElements[element]).style.visibility = "visible";
 	
 	for (element in uploadElements)
-	{	
-		document.getElementById(uploadElements[element]).style.visibility = "visible";
-	}
+        document.getElementById(uploadElements[element]).style.visibility = "visible";
 	
 	document.getElementById('btnSubmit').style.visibility = "visible";	
 	document.getElementById('btnSubmit').value = "Upload";
 
 }
+
+function validateComboBox(id, messageId)
+{
+    var valid = true;
+    
+    if(document.getElementById(id).selectedIndex == 0)
+    {   
+        document.getElementById(messageId).style.visibility = "visible";
+        valid = false;
+    }
+    else
+        document.getElementById(messageId).style.visibility = "hidden";
+        
+    return valid;
+}
+
+function validate()
+{
+    var valid = true;
+
+    if(document.getElementById('cboCustomer').selectedIndex == 0)
+        valid = false;
+        
+    if(document.getElementById('cboPartNumber').selectedIndex == 0)
+        valid = false;
+        
+    if(document.getElementById('cboRevision').selectedIndex == 0)
+        valid = false;
+        
+    if(document.getElementById('cboOperation').selectedIndex == 0)
+        valid = false;
+        
+    if(document.getElementById('cboMachine').selectedIndex == 0)
+        valid = false;
+        
+    if(document.getElementById('btnSubmit').value == "Upload")
+        if(document.getElementById('ofdGeometryFile').value == "")
+            if(document.getElementById('ofdCodeFile').value = "")
+                valid = false;
+        
+    return valid;
+}
+
